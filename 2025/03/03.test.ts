5db10cb98135ab21efacb07e7adc7b3d84b06383
Eemeli Söderholm <eemeli.s@gmail.com>
2025-12-04 23:49:50 +0200

    Solve 2025 03

---
+++ 2025/03/03.test.ts
@@ -0,0 +1,26 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { part1, part2 } from "./03.ts";
+
+const exampleInput = `
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+`.trim();
+const realInput = await Deno.readTextFile("./2025/03/03.txt");
+
+Deno.test("2025 Day 03 Part 1 - Example Input", () => {
+  assertEquals(part1(exampleInput), 357);
+});
+
+Deno.test("2025 Day 03 Part 1 - Real Input", () => {
+  assertEquals(part1(realInput), 17100);
+});
+
+Deno.test("2025 Day 03 Part 2 - Example Input", () => {
+  assertEquals(part2(exampleInput), 3121910778619);
+});
+
+Deno.test("2025 Day 03 Part 2 - Real Input", () => {
+  assertEquals(part2(realInput), 170418192256861);
+});
